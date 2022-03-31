df051e6c29a68e93cdb5d2d8d2af498b2fad66f7
Rohan Srivastava <notrohansrivastava@gmail.com>
2022-03-31 21:30:40 +0530

    generator v1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './style.css';
+import Header from './Header'
+import Meme from './Meme'
+
+function App() {
+  return (
+    <div className="App">
+    <Header/> 
+    <Meme/>    
+    </div>
+  );
+}
+
+export default App;
